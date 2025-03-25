a6176283922d69d0dff4faaf386cd4d245c0f425
kidneygood <2979564623@qq.com>
2025-03-25 16:42:11 +0800

    优化pointcloud_to_laserscan的配置参数

--- launch/start.py
+++ launch/start.py
@@ -2,6 +2,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
+# 直接定义常量
+pi = 3.141592653589793  # 定义圆周率
 
 def generate_launch_description():
     rviz_config = get_package_share_directory("rslidar_sdk") + "/rviz/rviz2.rviz"
@@ -28,18 +30,18 @@ def generate_launch_description():
         remappings=[("cloud_in", "/rslidar_points"), ("scan", "/rslidar_scan")],
         parameters=[
             {
-                "target_frame": "rslidar",
-                "transform_tolerance": 0.01,
-                "min_height": -0.4,
-                "max_height": 1.0,
-                "angle_min": -3.1415926,
-                "angle_max": 3.1415926,
-                "angle_increment": 0.003,
-                "scan_time": 0.1,
-                "range_min": 0.2,
-                "range_max": 100.0,
-                "use_inf": True,
-                "inf_epsilon": 1.0,
+                "target_frame": "laser_link",      # 目标坐标系
+                "transform_tolerance": 0.01,       # 坐标变换容差
+                "min_height": 0.2,                # 点云转换为激光扫描的最小高度
+                "max_height": 2.0,                # 点云转换为激光扫描的最大高度
+                "angle_min": -pi,                 # 扫描角度最小值
+                "angle_max": pi,                  # 扫描角度最大值
+                "angle_increment": 0.2/180*pi,    # 角度增量
+                "scan_time": 0.1,                 # 扫描时间
+                "range_min": 0.2,                 # 最小测量距离
+                "range_max": 200.0,               # 最大测量距离
+                "use_inf": False,                 # 是否使用无穷大值
+                "inf_epsilon": 0.5,               # 无穷大值的阈值，`use_inf=False`时实际上不起作用
             }
         ],
         name="pointcloud_to_laserscan",
@@ -47,8 +49,8 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            rsildar_sdk_node,
-            rviz2_node,
+            rsildar_sdk_node,            
             pointcloud_to_laserscan_node,
+            rviz2_node,
         ]
     )
